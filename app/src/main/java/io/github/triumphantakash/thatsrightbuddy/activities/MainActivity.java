0db4a378fb7ce28f605cdf200c61eff17cbf1d76
Akash Chaturvedi <axc144430@utdallas.edu>
2016-03-05 16:25:55 -0600

    writing data from SocketReaderThread to the Main Thread using Looper

--- app/src/main/java/io/github/triumphantakash/thatsrightbuddy/activities/MainActivity.java
+++ app/src/main/java/io/github/triumphantakash/thatsrightbuddy/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package io.github.triumphantakash.thatsrightbuddy.activities;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -17,17 +21,33 @@ import io.github.triumphantakash.thatsrightbuddy.threads.ClientSocketWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-    //sample comment from devMayank branch
-    String sentence;
-    String modifiedSentence;
-    BufferedReader inFromUser;
     Socket clientSocket;
     DataOutputStream outToServer;
     BufferedReader inFromServer;
+    TextView myText, friendsText;
+    Button sendButton;
+    Handler mHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myText = (TextView)findViewById(R.id.text_my);
+        friendsText = (TextView)findViewById(R.id.text_friends);
+        sendButton = (Button)findViewById(R.id.button_send);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Looper.myLooper() == null) {
+                    Looper.prepare();
+                }
+                mHandler = new Handler() {
+                    public void handleMessage(String msg) {
+                        friendsText.append(msg);
+                    }
+                };
+            }
+        });
 
         try {
             clientSocket = new Socket("192.168.0.26", 6969);
@@ -37,10 +57,22 @@ public class MainActivity extends AppCompatActivity {
             socketWriteThread.start();
 
             inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            ClientSocketReader socketReadThread = new ClientSocketReader(inFromServer);
+            ClientSocketReader socketReadThread = new ClientSocketReader(inFromServer, mHandler);
             socketReadThread.start();
+
         }catch (IOException e){
             Toast.makeText(getApplicationContext(), "could not connect to the server", Toast.LENGTH_SHORT).show();
         }
+
     }
+
+    //send data to Writer thread in onResume function and also
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+
 }
--- app/src/main/java/io/github/triumphantakash/thatsrightbuddy/threads/ClientSocketReader.java
+++ app/src/main/java/io/github/triumphantakash/thatsrightbuddy/threads/ClientSocketReader.java
@@ -1,5 +1,11 @@
 package io.github.triumphantakash.thatsrightbuddy.threads;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
@@ -9,21 +15,31 @@ import java.io.IOException;
 //reads from the server and display messages on user's screen
 public class ClientSocketReader extends Thread{
     BufferedReader inFromServer;
+    Message msg;
     String message;
-    public ClientSocketReader(BufferedReader inFromServer) {
+    Handler referenceHandler;
+    Bundle bundle;
+    public ClientSocketReader(BufferedReader inFromServer, Handler handler) {
         this.inFromServer = inFromServer;
+        this.referenceHandler = handler;
     }
     public void run(){
+        Looper.prepare();
+        msg = new Message();
         try {
             System.out.println("client reader thread is up and running");
             while(true){
                 Thread.sleep(1000);
                 message = inFromServer.readLine();	//a new message is arrived
-                System.out.println("got this message from main server: "+message+"\n");
+                bundle.putString("FRIEND: ", message);
+                msg.setData(bundle);
+                referenceHandler.sendMessage(msg);
+                Log.i("****TAG****", "SERVER: " + message + "\n");
             }
         } catch (IOException | InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        Looper.loop();
     }
 }
