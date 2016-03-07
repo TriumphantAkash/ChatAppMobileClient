69175c8d1725c1c741fa99f3ceac2f75e374d028
Akash Chaturvedi <axc144430@utdallas.edu>
2016-03-07 14:22:21 -0600

    added Settings Activity and launcher Icon to that from main activity

--- app/src/main/java/io/github/triumphantakash/thatsrightbuddy/activities/MainActivity.java
+++ app/src/main/java/io/github/triumphantakash/thatsrightbuddy/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package io.github.triumphantakash.thatsrightbuddy.activities;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -72,6 +75,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+           // return true;
+            toSettings();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void toSettings()
+    {
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(intent);
     }
 
     //reads from user input and write it to server stream
